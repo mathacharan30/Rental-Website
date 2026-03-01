04db25ef88b98a72707e71d7df618cc29bb13278
Matha Charan <mathacharan3004@gmail.com>
2026-03-01 14:13:10 +0530

    some changes

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
